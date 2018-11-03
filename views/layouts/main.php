c5e45ff9215ce24556d7488eabe448469d39edf5
Your Name <you@example.com>
2018-11-03 12:38:09 +0500

    Merge origin/master

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,6 +38,7 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
+            ['label' => 'Книги', 'url' => ['/book/index']],
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
