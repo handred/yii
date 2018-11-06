563c3c8606ddff0f9fab4c5fe09fb2834a2a13ed
Your Name <you@example.com>
2018-11-06 08:45:04 +0500

    Merge origin/master

--- models/Book.php
+++ models/Book.php
@@ -5,10 +5,12 @@ namespace app\models;
 use \yii;
 use \yii\db\ActiveRecord;
 use \yii\web\UploadedFile;
-
-
+use yii\imagine\Image;
 
 class Book extends ActiveRecord {
+
+    const upload_dir = 'uploads/';
+    const upload_dir_small = 'uploads/thumb';
 
     public static function tableName() {
         return 'book';
@@ -30,15 +32,27 @@ class Book extends ActiveRecord {
             return false;
         }
         $photo = $this->photo->baseName . '.' . $this->photo->extension;
-        $this->photo->saveAs('uploads/' . $photo);
+
+        if (!is_dir(self::upload_dir)) {
+            mkdir(self::upload_dir);
+        }
+
+        if (!is_dir(self::upload_dir_small)) {
+            mkdir(self::upload_dir_small);
+        }
+
+        $this->photo->saveAs(self::upload_dir . $photo);
+
+        Image::thumbnail(self::upload_dir . $photo, 50, 50)->save(self::upload_dir_small . $photo, ['quality' => 95]);
+
         return $photo;
     }
 
     public function getimage() {
-        if (!is_file('uploads/' . $this->photo)) {
-            return '/uploads/nofoto.jpg';
+        if (!is_file(self::upload_dir . $this->photo)) {
+            return '/' . self::upload_dir . '/nofoto.jpg';
         }
-        return '/uploads/'.$this->photo;
+        return '/' . self::upload_dir . $this->photo;
     }
- 
+
 }
