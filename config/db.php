97523baf20776f660c5f3511c28901416c30392a
Your Name <you@example.com>
2019-02-10 20:51:07 +0500

    m

--- config/db.php
+++ config/db.php
@@ -6,9 +6,10 @@ return [
     'username' => 'root',
     'password' => '1234',
     'charset' => 'utf8',
+    
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+//    'enableSchemaCache' => true,
+//    'schemaCacheDuration' => 60,
+//    'schemaCache' => 'cache',
 ];
--- modules/items/views/index/_form.php
+++ modules/items/views/index/_form.php
@@ -2,10 +2,16 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+
+use app\modules\pages\models\Pages;
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\items\models\Items */
 /* @var $form yii\widgets\ActiveForm */
+
+
+$items = ArrayHelper::map(Pages::find()->all(),'id','title');
 ?>
 
 <div class="items-form">
@@ -16,7 +22,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'page_id')->textInput() ?>
+    <?= $form->field($model, 'page_id')->dropDownList($items, ['prompt'=>'Выберите раздел']) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
