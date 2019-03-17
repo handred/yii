bd2cba59997c97a4184cf51ea5d956f585a251af
Your Name <you@example.com>
2019-03-17 18:29:24 +0500

    m

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii',
     'username' => 'root',
-    'password' => '1234',
+    'password' => 'pilorama73',
     'charset' => 'utf8',
     
 
